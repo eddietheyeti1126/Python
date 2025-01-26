4dcc7100ade12f269707ea958d98ac67ae53e34d
eddietheyeti1126 <emanges@students.mchenry.edu>
2025-01-25 18:45:17 -0600

    Add files via upload

---
+++ Knock Knock.py
@@ -0,0 +1,7 @@
+print("Knock Knock")
+print("Whos there")
+print("Lettuce")
+print("Lettuce who")
+print("Lettuce in its cold outside")
+
+print("Knock Knock\nWhos there\nLettuce\nLettuce who\nLettuce in its cold outside")
