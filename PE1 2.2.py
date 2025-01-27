12e6c49f4a65c7d7640c357da0b03d7deafa33fb
eddietheyeti1126 <emanges@students.mchenry.edu>
2025-01-27 12:41:58 -0600

    PE1 2.2

---
+++ PE1 2.2.py
@@ -0,0 +1,22 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 50
+print("Literal:", x)
+
+# Integer
+y = -100
+print("Integer:", y)
+
+# Float
+z = 25.3
+print("Float:", z)
+
+# String
+name = "Eddie"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
