7d4bd68e75c22adfacd4444f5c4afa2c4c345f67
eddietheyeti1126 <emanges@students.mchenry.edu>
2025-02-02 16:47:15 -0600

    Add files via upload

---
+++ PE1 2.5.py
@@ -0,0 +1,17 @@
+def birthday():
+    """
+    Returns a string with the user's birthday in the format:
+    'Your birthday is Month Day, Year.'
+    """
+    return "Your birthday is July 8, 1998."
+
+def address():
+    """
+    Returns a string with the user's address in the format:
+    'Your address is Street, City, State, ZIP Code.'
+    """
+    return "Your address is 123 Main St, Crystal Lake, IL, 60013."
+
+# Print the results
+print(birthday())
+print(address())
