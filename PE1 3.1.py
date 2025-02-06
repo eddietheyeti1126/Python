a54540963f43c677d450964b31440c5c9216397d
eddietheyeti1126 <emanges@students.mchenry.edu>
2025-02-05 22:59:40 -0600

    Add files via upload

---
+++ PE1 3.1.py
@@ -0,0 +1,44 @@
+#- or +
+number = -10
+if number >0:
+    print("The number is positive")
+elif number <0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+#age to drive
+age = 23
+if age >15:
+    print("They can drive")
+elif age <15:
+    print("They can't drive")
+else:
+    print("They can't drive")
+# voting age
+voteage = 15
+if voteage >18:
+    print("They can vote")
+elif voteage <18:
+    print("They can't vote")
+else:
+    print("They can vote")
+# age to buy alcohol
+alcoholage = 12
+if alcoholage >21:
+    print("They can buy alcohol")
+elif alcoholage <21:
+    print("They can't by alcohol")
+else:
+    print("They can buy alcohol")
+# age to apply for a senior discount
+seniorage = 72
+if seniorage >65:
+    print("They apply for a senior discount")
+elif seniorage <65:
+    print("They don't apply for a senior discount")
+else:
+    print("They apply for a senior discount")
+
+
+
+
