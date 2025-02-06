6f48fcb63b15d0a9fb0caed3a25ebc1cd9708db6
eddietheyeti1126 <emanges@students.mchenry.edu>
2025-02-06 10:54:53 -0600

    Add files via upload

---
+++ PE1 3.1 B.py
@@ -0,0 +1,18 @@
+score = 95
+# score for F
+if score <60:
+    print("F")
+# score for D
+elif score <70:
+    print("D")
+# score for C
+elif score <80:
+    print("C")
+# score for B
+elif score <90:
+    print("B")
+# score for A
+elif score <100:
+    print("A")
+else:
+    print("A")
