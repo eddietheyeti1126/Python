f52b71448004edd0d56328877b4d51dabdd46e3e
eddietheyeti1126 <emanges@students.mchenry.edu>
2025-04-23 17:32:10 -0500

    Add files via upload

---
+++ file_organizer.py
@@ -0,0 +1,11 @@
+import os
+
+# Create main folder
+os.mkdir("MyFiles")
+
+# Create subfolders
+os.mkdir("MyFiles/Docs")
+os.mkdir("MyFiles/Images")
+os.mkdir("MyFiles/Videos")
+
+print("Folders created.")
